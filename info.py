191aaefdf8c4163b25fcaf151478b8d2b224a79a
JN2FLIX <99884442+Jnanesh124@users.noreply.github.com>
2025-01-31 19:47:25 +0600

    Update info.py

--- info.py
+++ info.py
@@ -79,7 +79,7 @@ PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b>- ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʟ�
 
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
 CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
-CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "mongodb+srv://strong:strong@cluster0.ix7usa3.mongodb.net/?retryWrites=true&w=majority") # Necessary If clone mode is true
+CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone mode is true
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'JN2FLIX_KANNADA') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 
