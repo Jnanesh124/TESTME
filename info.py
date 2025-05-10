8f9aeea3c495df32cc1f5786a2e0e7f7bb97fe4a
JN2FLIX <99884442+Jnanesh124@users.noreply.github.com>
2025-05-10 15:24:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -115,8 +115,8 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
 VERIFY = bool(environ.get('VERIFY', True))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'seturl.in')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '3daf41670bf9ee8030e786aed791f15ffb7eb104')
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'arolinks.com')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'dc64f71dee43e715379e1da5d3bc9dbeb96b71a7')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', '')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
