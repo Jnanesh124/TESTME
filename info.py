cdf7773271f67241952c4e76816e8888e8f6135d
JN2FLIX <99884442+Jnanesh124@users.noreply.github.com>
2025-08-25 16:37:08 +0600

    Update info.py

--- info.py
+++ info.py
@@ -90,10 +90,10 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'JN2FLIX_KANNADA') # Pu
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/JNKBACKUP')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/JNKBACKUP')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/JNK_BACKUP')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/JNK_BACKUP')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 't.me/+JS5D7Oi3CkQ0NTBl') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/JNKBACKUP')
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/JNK_BACKUP')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
@@ -198,4 +198,5 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
