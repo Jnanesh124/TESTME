012c392f6b1033b27a3fbf20f9ebc44d3a4780c9
JN2FLIX <99884442+Jnanesh124@users.noreply.github.com>
2025-09-11 18:21:22 +0000

    ok

--- info.py
+++ info.py
@@ -110,7 +110,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 't.me/+JS5D7Oi3CkQ0NTBl') # Support C
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/JNK_BACKUP')
 
 BAD_WORDS = [
-    "2",
+    "3",
     "@MovieShopLinks",
     "UNTOUCHED",
     "AVC",
