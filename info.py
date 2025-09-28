d2dcd8ad40e185aa17e2a9f9674d2dbb97f51a2b
JN2FLIX <99884442+Jnanesh124@users.noreply.github.com>
2025-09-28 09:30:28 +0000

    ok

--- info.py
+++ info.py
@@ -110,6 +110,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 't.me/+JS5D7Oi3CkQ0NTBl') # Support C
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/JNK_BACKUP')
 
 BAD_WORDS = [
+    "op",
     "ok",
     "jnanesh",
     "3",
@@ -122,8 +123,8 @@ BAD_WORDS = [
     "HQ",
     "@RM_Movie_Flix",
     "@JNK_BACKUP",
-    "[",
-    "]",
+    "["
+] # List of bad words to filter out - Can be modified at runtime using /addbadword and /removebadword commands",
     "www.",
     "@",
     "@ROCKERSBACKUP",
